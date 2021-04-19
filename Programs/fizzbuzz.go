009109d869cf93f823fc44021f8db9c24ed244ff
Bavan M Y <70379421+bavan172@users.noreply.github.com>
2021-04-19 08:24:40 +0530

    Create fizzbuzz.go

--- Programs/fizzbuzz.go
+++ Programs/fizzbuzz.go
@@ -3,7 +3,6 @@
 // For each multiple of 5, print "Buzz" instead of the number. 
 // For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
-
 package main
 
 import "fmt"
